d88e6f018526f101f0089201bffc0240b952d8aa
LeoGytis <leogytis@gmail.com>
2023-07-12 15:25:26 +0300

    goood boy

--- src/app/components/Products.tsx
+++ src/app/components/Products.tsx
@@ -28,15 +28,15 @@ const ProductsArray = [
 
 const Products: any = () => {
   return (
-    <div className="w-full flex flex-col md:flex-row justify-between bg-white p-8">
-      <div className="flex flex-col justify-center items-center md:items-start pb-4 md:pb-0 text-slate-600">
+    <div className="w-full flex flex-col md:flex-row justify-between bg-white p-4">
+      <div className="flex md:w-1/5 flex-col justify-center items-center md:items-start pb-4 md:pb-0 text-slate-600">
         <h1 className="text-xl">Our products</h1>
         <h3 className="text-sm underline underline-offset-2">Assortment</h3>
       </div>
-      <div className="flex flex-col md:flex-row justify-around">
+      <div className="flex md:w-4/5 flex-col md:flex-row justify-around">
         {ProductsArray.map((product) => (
           <div className="flex flex-col items-center align-center m-4">
-            <div className="flex h-20 w-20 md:h-28 md:w-28 lg:h-36 lg:w-36 pb-4">
+            <div className="flex h-32 w-28 md:h-20 md:w-12 lg:h-28 lg:w-16 pb-4 ">
               <img src={product.img} alt={product.title} />
             </div>
             <div className="text-sm text-center text-slate-600">
