ef176ea6985712ec67e02d867a8e70bddf6021ab
LeoGytis <leogytis@gmail.com>
2023-07-12 15:16:12 +0300

    products is good

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,7 +1,7 @@
 const NavBar: any = () => {
   return (
     <div className="bg-[#1C3C87]">
-      <div className="max-w-7xl flex flex-col md:flex-row items-center justify-between  p-4 px-8">
+      <div className="w-full flex flex-col md:flex-row items-center justify-between p-4 px-8">
         <div className="flex items-center w-200 sm:w-[320px] pb-4 md:pb-0">
           <img src="/logo.png" alt="logo" />
         </div>
--- src/app/components/Products.tsx
+++ src/app/components/Products.tsx
@@ -2,46 +2,49 @@ const ProductsArray = [
   {
     id: 484848,
     title: "Cheese Sticks",
-
-    img: "https://placehold.co/100x200",
+    img: "/cheese_sticks.png",
   },
   {
     id: 797979,
     title: "Yogurt",
-    img: "https://placehold.co/100x200",
+    img: "/yogurt.png",
   },
   {
     id: 797979,
     title: "Flavored milk drinks",
-    img: "https://placehold.co/100x200",
+    img: "/milk_drinks.png",
   },
   {
     id: 797979,
     title: "Desserts",
-    img: "https://placehold.co/100x200",
+    img: "/desserts.png",
   },
   {
     id: 797979,
     title: "Ice cream",
-    img: "https://placehold.co/100x200",
+    img: "/ice_cream.png",
   },
 ];
 
 const Products: any = () => {
   return (
-    <div className="max-w-7xl flex justify-between border border-orange-600">
-      <div className="flex-col items-center">
-        <h1>Our products</h1>
-        <h3>Assortment</h3>
+    <div className="w-full flex flex-col md:flex-row justify-between bg-white p-8">
+      <div className="flex flex-col justify-center items-center md:items-start pb-4 md:pb-0 text-slate-600">
+        <h1 className="text-xl">Our products</h1>
+        <h3 className="text-sm underline underline-offset-2">Assortment</h3>
       </div>
-      {ProductsArray.map((product) => (
-        <>
-          <div>
-            <img src="{product.img}" alt="${product.title}" />
+      <div className="flex flex-col md:flex-row justify-around">
+        {ProductsArray.map((product) => (
+          <div className="flex flex-col items-center align-center m-4">
+            <div className="flex h-20 w-20 md:h-28 md:w-28 lg:h-36 lg:w-36 pb-4">
+              <img src={product.img} alt={product.title} />
+            </div>
+            <div className="text-sm text-center text-slate-600">
+              {product.title}
+            </div>
           </div>
-          <div>{product.title}</div>
-        </>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
