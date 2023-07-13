ca9bf1d4e6646590687f01177d2c25f945761680
LeoGytis <leogytis@gmail.com>
2023-07-13 13:26:43 +0300

    getting that css done

--- src/app/components/Prod.tsx
+++ src/app/components/Prod.tsx
@@ -58,28 +58,47 @@ const Prod: React.FC = () => {
       </div>
       {Object.keys(selectedProduct).length > 0 && (
         <div className="fixed top-0 left-0 right-0 bottom-0 z-50 bg-black bg-opacity-50 flex justify-start items-center">
-          <div className="bg-white border-2 border-orange-500 p-8 w-3/4 h-full transform -translate-x-1/5 transition-transform duration-300">
-            <div className="flex justify-end">
-              <button className="text-3xl p-2" onClick={handleCloseSlide}>
+          <div className="bg-white w-3/4 h-full transform -translate-x-1/5 transition-transform duration-1000 animate-slide-in">
+            <div className="flex justify-end pt-2 pr-4">
+              <button
+                className="text-lg font-semibold text-gray-300 transition-all duration-300 hover:text-black"
+                onClick={handleCloseSlide}
+              >
                 &#10005;
               </button>
             </div>
-            <div className="flex h-32 w-28 md:h-20 md:w-12 lg:h-28 lg:w-16 pb-4">
-              <img src={selectedProduct.img} alt={selectedProduct.title} />
-            </div>
-            <div className="text-sm text-slate-600">
-              <h1 className="text-2xl mb-4">{selectedProduct.title}</h1>
-              <p>{selectedProduct.description_header}</p>
-              <p>Subtitle: {selectedProduct.subtitle}</p>
-              <p>Awards: {selectedProduct.awards.join(", ")}</p>
-              <p>Energy Value: {selectedProduct.energy_value.value}</p>
-              <p>Fats: {selectedProduct.energy_value.fats}</p>
-              <p>Fats More: {selectedProduct.energy_value.fats_more}</p>
-              <p>Carbs: {selectedProduct.energy_value.carbs}</p>
-              <p>Carbs Sugar: {selectedProduct.energy_value.carbs_sugar}</p>
-              <p>Proteins: {selectedProduct.energy_value.proteins}</p>
-              <p>Salt: {selectedProduct.energy_value.salt}</p>
-              <p>Description: {selectedProduct.description}</p>
+            <div className="flex h-full">
+              <div className="flex w-1/2 p-40 border-2 border-lime-600">
+                <img src={selectedProduct.img} alt={selectedProduct.title} />
+              </div>
+              <div className="w-1/2 text-sm text-slate-600 p-20 border-2 border-orange-500">
+                <h1 className="text-3xl leading-3 font-bold text-[#113b85] mb-4">
+                  {selectedProduct.title}
+                </h1>
+                <div className="text-sm text-[#808080]">
+                  {selectedProduct.subtitle}
+                </div>
+                <div className="py-6">
+                  Awards: {selectedProduct.awards.join(", ")}
+                </div>
+                <div className="font-bold text-[#113b85]">
+                  Energetinė vertė (100g):
+                  <p className="font-normal text-[#808080]">
+                    {selectedProduct.energy_value.value}
+                  </p>
+                </div>
+                <div>Fats: {selectedProduct.energy_value.fats}</div>
+                <div>Fats More: {selectedProduct.energy_value.fats_more}</div>
+                <div>Carbs: {selectedProduct.energy_value.carbs}</div>
+                <div>
+                  Carbs Sugar: {selectedProduct.energy_value.carbs_sugar}
+                </div>
+                <div>Proteins: {selectedProduct.energy_value.proteins}</div>
+                <div>Salt: {selectedProduct.energy_value.salt}</div>
+                <div className="font-bold text-[#113b85]">
+                  Produkto aprašymas: {selectedProduct.description}
+                </div>
+              </div>
             </div>
           </div>
         </div>
