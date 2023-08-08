d4b8a43b0fca889da100ac921e20da5617295f71
LeoGytis <leogytis@gmail.com>
2023-08-08 13:47:57 +0300

    on side close button

--- src/app/components/ProductSlide1.tsx
+++ src/app/components/ProductSlide1.tsx
@@ -7,7 +7,10 @@ export interface ProductSlideProps {
 
 const ProductSlide1 = ({ onClose }: ProductSlideProps) => {
   return (
-    <div className="fixed top-0 left-0 right-0 bottom-0 z-50 bg-black bg-opacity-50 flex justify-start items-center">
+    <div
+      className="fixed top-0 left-0 right-0 bottom-0 z-50 bg-black bg-opacity-50 flex justify-start items-center"
+      onClick={onClose}
+    >
       <div
         className="bg-white md:w-11/12 h-full overflow-auto transform transition-transform duration-1000 animate-product-slide"
         onClick={(event) => event.stopPropagation()}
