53b933e21bb765010932cf1217cb45af2057c1d4
Gytis <101097792+LeoGytis@users.noreply.github.com>
2023-08-28 15:00:23 +0300

    img edit

--- src/app/private_label/page.tsx
+++ src/app/private_label/page.tsx
@@ -15,8 +15,6 @@ const PrivateProducts = () => {
           </h3>
         </div>
         <div className="flex gap-6 md:w-4/5 flex-col md:flex-row justify-around items-center content-center px-8">
-         
-
           {/* ====================== Product No.1 ====================== */}
           <div className="flex flex-col hover:cursor-pointer items-center">
             <div className="flex h-32 w-20 md:h-20 md:w-12 lg:h-32 lg:w-20 pb-4">
@@ -32,8 +30,8 @@ const PrivateProducts = () => {
             </div>
           </div>
 
-           {/* ====================== Product No.2 ====================== */}
-           <div className="flex flex-col  hover:cursor-pointer h-full justify-between items-center">
+          {/* ====================== Product No.2 ====================== */}
+          <div className="flex flex-col  hover:cursor-pointer h-full justify-between items-center">
             <div className="flex h-20 w-32 md:h-20 md:w-28 lg:w-36 lg:mt-6">
               <Image
                 src="/images/private_products/mozzarella.png"
@@ -47,7 +45,7 @@ const PrivateProducts = () => {
 
           {/* ====================== Product No.3 ====================== */}
           <div className="flex flex-col  hover:cursor-pointer h-full justify-between items-center">
-          <div className="flex h-20 w-32 md:h-12 md:w-24 lg:h-16 lg:w-32 md:mt-4 lg:mt-6">
+            <div className="flex h-20 w-32 md:h-10 md:w-24 lg:h-12 lg:w-32 md:mt-4 lg:mt-6">
               <Image
                 src="/images/private_products/ice_cream.png"
                 alt="ice_cream"
@@ -85,7 +83,9 @@ const PrivateProducts = () => {
                 style={{ objectFit: "contain" }}
               />
             </div>
-            <div className="text-sm text-center text-slate-600">Iced coffee drinks</div>
+            <div className="text-sm text-center text-slate-600">
+              Iced coffee drinks
+            </div>
           </div>
         </div>
       </div>
