127c2e1fdc598a6091dbf35b22e9751b43cf9477
LeoGytis <leogytis@gmail.com>
2023-09-04 15:18:32 +0300

    link to

--- src/app/components/Products.tsx
+++ src/app/components/Products.tsx
@@ -37,9 +37,12 @@ const Products = () => {
       <div className="w-full md:max-h-64 flex flex-col md:flex-row justify-between bg-white p-7 pt-12 pb-14">
         <div className="flex md:w-1/5 flex-col gap-4 justify-center items-center md:items-start md:pl-8 pb-10 md:pb-0">
           <h1 className="text-2xl text-[#616161] font-medium">Our products</h1>
-          <h3 className="italic text-[#1B3C80] underline underline-offset-2">
+          <a
+            className="italic text-[#1B3C80] underline underline-offset-2"
+            href="https://pienozvaigzdes.lt/com/en/2-home"
+          >
             Assortment
-          </h3>
+          </a>
         </div>
         <div className="flex gap-6 md:w-4/5 md:h-4/5 flex-col md:flex-row justify-around items-center content-center">
           {/* ====================== Product No.1 ====================== */}
